a96092b58170d077a613940aa9e5bc92357e6865
LeaffLovesBingus <mati.sotitol@gmail.com>
2025-01-03 17:01:14 -0300

    intento #2 setup sentry

--- Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.edge.config.ts
+++ Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.edge.config.ts
@@ -3,14 +3,14 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs"
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://023d5e450bb45e1a392d3c707e0a86fa@o4507363988733952.ingest.us.sentry.io/4507363992469504",
+  dsn: "https://38e23d6aba14a138ab4db57e1570b323@o4508520974123008.ingest.us.sentry.io/4508581180997632",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-})
+});
