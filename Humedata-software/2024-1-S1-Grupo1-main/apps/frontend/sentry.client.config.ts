8fe64f49294dfd85a2163771efaf9053b6a813f0
LeaffLovesBingus <mati.sotitol@gmail.com>
2025-01-03 19:21:55 -0300

    Revert "intento #2 setup sentry"
    This reverts commit a96092b58170d077a613940aa9e5bc92357e6865.

--- Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.client.config.ts
+++ Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.client.config.ts
@@ -2,27 +2,29 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-  dsn: "https://38e23d6aba14a138ab4db57e1570b323@o4508520974123008.ingest.us.sentry.io/4508581180997632",
+  dsn: "https://023d5e450bb45e1a392d3c707e0a86fa@o4507363988733952.ingest.us.sentry.io/4507363992469504",
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  replaysOnErrorSampleRate: 1.0,
+
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+  integrations: [
+    Sentry.replayIntegration({
+      // Additional Replay configuration goes in here, for example:
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+})
--- Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.server.config.ts
+++ Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/sentry.server.config.ts
@@ -2,14 +2,18 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from "@sentry/nextjs"
 
 Sentry.init({
-  dsn: "https://38e23d6aba14a138ab4db57e1570b323@o4508520974123008.ingest.us.sentry.io/4508581180997632",
+  dsn: "https://023d5e450bb45e1a392d3c707e0a86fa@o4507363988733952.ingest.us.sentry.io/4507363992469504",
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+})
