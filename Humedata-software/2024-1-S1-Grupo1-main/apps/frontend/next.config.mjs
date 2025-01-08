b1f146f45911952815ec20742e48ddf844ae61d5
LeaffLovesBingus <mati.sotitol@gmail.com>
2025-01-07 21:32:30 -0300

    intento #4 setup sentry

--- Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/next.config.mjs
+++ Humedata-software/2024-1-S1-Grupo1-main/apps/frontend/next.config.mjs
@@ -8,8 +8,8 @@ export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "cehum",
-project: "humedatos-nextjs",
+org: "matias-soto-lomboy",
+project: "cehum-humedata",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
